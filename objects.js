baf8764353001c8ef391a1ba5543b35c42919d91
John Tuttle <thejwt@gmail.com>
2017-09-12 17:50:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = new Object();
 playlist.kanye = 'school spirit';
 
 function updatePlaylist(playlist,artistName,songTitle){
-   Object.assign({}, playlist, {[artistName]: songTitle})
+   Object.assign({}, playlist, artistName, songTitle)
 }
 
 function removeFromPlaylist(playlist,artistName){
