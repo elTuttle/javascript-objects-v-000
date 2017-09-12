342578fcaa7b35dd41d59b96c4294356bfd42f99
John Tuttle <thejwt@gmail.com>
2017-09-12 17:56:55 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,8 @@ function updatePlaylist(playlist,artistName,songTitle){
    return playlist
 }
 
+console.log(updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi"));
+
 function removeFromPlaylist(playlist,artistName){
   delete playlist.artistName;
   return playlist
